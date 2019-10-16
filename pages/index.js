5df2f80b3505b1c70e68c73eb2d65a0c58ecf2de
Wilhelm Willie <WilhelmWillie@gmail.com>
2019-10-16 11:21:07 -0700

    [Refactor] Site cleanup and SEO improvements (#8)
    * Code cleanup, SEO improvements
    
    * Moved meta tags to different component

--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,11 @@
-import Head from "next/head";
-
-import styled from "styled-components";
-
-import { Header, Hero } from "../components";
+import { Navbar, Hero, MetaTags } from "../components";
 
 const Landing = () => {
   return (
     <>
-      <Head>
-        <title>HackSC 2020</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta
-          name="description"
-          content="HackSC 2020. USC's premier hackathon, coming back soon."
-        ></meta>
+      <MetaTags title="HackSC 2020 - USC's largest hackathon" />
 
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=UA-127488741-1"
-        ></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'UA-127488741-1');
-          `
-          }}
-        />
-
-        <link rel="icon" type="image/png" href="/static/favicon.png" />
-      </Head>
-
-      <Header />
+      <Navbar />
       <Hero />
     </>
   );
