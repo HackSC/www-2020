2634fe49b7518656e0de5e3937fb86ad71066bcf
Wilhelm Willie <WilhelmWillie@gmail.com>
2019-10-17 17:50:47 -0700

    [Feat] Added verticals/social justice section (#11)
    * Set up cards for verticals
    
    * Mobile fix
    
    * Small verticals header change

--- components/index.js
+++ components/index.js
@@ -2,3 +2,4 @@ export { default as Navbar } from "./Navbar";
 export { default as Hero } from "./Hero";
 export { default as MetaTags } from "./MetaTags";
 export { default as Details } from "./Details";
+export { default as Verticals } from "./Verticals";
